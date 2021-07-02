5f91aa7985de85753b091c21905db771952abae6
arjunamarcelino <13519021@std.stei.itb.ac.id>
2021-07-02 14:24:55 +0700

    feat: add profil

---
+++ application/views/profil.php
@@ -0,0 +1,53 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <style type="text/css">
+        .back {
+            background-color: #1950A3;
+            position: relative;
+            left: 0;
+            top: 2rem;
+        }
+
+        .container {
+            padding-left: 5rem;
+            padding-right: 5rem;
+            color: white;
+            font-family: 'Roboto';
+        }
+
+        .square {
+            height: 9rem;
+            width: 15em;
+            background-color: white;
+            border-radius: 0.5rem;
+            display: inline-block;
+            margin-left: 2rem;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="back">
+        <div class="container">
+            <h1 style="font-size: 30px; padding-top:2rem;">What is Trans Retail Indonesia?</h1>
+            <!-- profil content here -->
+            <div style="padding-bottom: 2rem;">
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
