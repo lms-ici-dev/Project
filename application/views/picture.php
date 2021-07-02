c4b1c1d99d824af15aa1c8270d05c98131a43ad1
arjunamarcelino <13519021@std.stei.itb.ac.id>
2021-07-02 14:23:53 +0700

    feat: add picture promo

---
+++ application/views/picture.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <style>
+        /* width */
+        ::-webkit-scrollbar {
+            width: 10px;
+        }
+
+        /* Track */
+        ::-webkit-scrollbar-track {
+            background: #f1f1f1;
+        }
+
+        /* Handle */
+        ::-webkit-scrollbar-thumb {
+            background: #888;
+        }
+
+        /* Handle on hover */
+        ::-webkit-scrollbar-thumb:hover {
+            background: #555;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    echo "<img src='./././assets/Banner.jpg' alt='photo of me' style='margin-top: 4rem;width:100%; background-color: white';overflow: hidden;/>";
+    ?>
+</body>
+
+</html>
